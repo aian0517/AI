c74881d4faf7a3bf5d720e0f3e18b19dc0261372
aian0517 <134360677+aian0517@users.noreply.github.com>
2024-03-22 11:49:23 +0800

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 const body1_card_data = {
     'body1_btn': [
         {
-            img: '',
+            img: './images/AI/10.jpg',
             title: 'AI是什麼',
             text: '人工智慧(AI)是電腦科學的一個領域，致力於解決與人類智慧相關的常見認知問題，例如學習、創造和影像辨識。\
             AI 的目標是建立能從資料中取得有用資訊的自學習系統，然後AI 可以套用這些資訊以仿似人類的方式解決新問題。',
@@ -33,44 +33,44 @@ const body1_card_data = {
     ],
     'body1_btn3':[
         {
-            img:'',
+            img:'./images/AI/body1_btn3_1.jpg',
             title:'語音辨識',
             text:'自動將語音轉成書面文字',
         },
         {
-            img:'',
+            img:'./images/AI/body1_btn3_2.jpg',
             title:'圖片辨識',
             text:'辨別圖片的各個層面並加以分類。',
         },
         {
-            img:'',
+            img:'./images/AI/body1_btn3_3.jpg',
             title:'智慧文件處理',
             text:'智慧文件處理(IDP)可將非結構化文件格式轉換為可用的資料',
         },
         {
-            img:'',
+            img:'./images/AI/body1_btn3_4.jpg',
             title:'商業分析',
             text:'商業分析領域使用AI來收集、處理和分析複雜的資料集。',
         },
     ],
     'body1_btn4':[
         {
-            img:'',
+            img:'./images/AI/body1_btn4_1.svg',
             title:'Midjourney',
             text:'一個由位於美國加州舊金山的同名研究實驗室開發之人工智慧程式，可根據文字生成圖像',
         },
         {
-            img:'',
+            img:'./images/AI/body1_btn4_2.png',
             title:'Simplified',
             text:'註冊一個免費帳號後，便能開始使用。其中一個頗特殊的功能是自動生成文案',
         },
         {
-            img:'',
+            img:'./images/AI/body1_btn4_3.png',
             title:'Runway',
             text:'家總部位於紐約市的美國公司，專門從事生成人工智慧研究和技術。該公司主要專注於創建用於生成影片、圖像和各種多媒體內容的產品和模型。',
         },
         {
-            img:'',
+            img:'./images/AI/body1_btn4_4.png',
             title:'Phrasly',
             text:'這是一款全方位的AI生成文本修改器，您可以將您的AI生成內容轉化為與人類書寫非常相似的內容',
         },
